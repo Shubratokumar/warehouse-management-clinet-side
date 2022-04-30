d42a297234e27aae3f87cd9d8189cc61a5b8ef67
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-30 09:06:25 +0600

    firebase deploy and basic folder structure done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Route, Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home/Home";
 
 
 function App() {
   return (
     <div>
-      <Router>
+      <Routes>
         <Route path="/" element={<Home/>}></Route>
-      </Router>
+      </Routes>
     </div>
   );
 }
