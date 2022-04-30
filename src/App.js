62b5fe7c22a6e8c1d5900095c5d6d7c70c06f758
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-30 21:02:33 +0600

    404 page design done.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home/Home";
 import Header from "./pages/Shared/Header/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NotFound from "./pages/NotFound/NotFound";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Routes>      
         <Route path="/" element={<Home/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </div>
   );
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Error from "../../../src/Assets/Images/404.jpg";
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>this is 404 page</h2>
+        <div className='bg-warning'>
+            <div className="container">
+                <div className="row g-4 align-items-center justify-content-center">
+                    <div className="col-sm-12 col-lg-6">
+                        <h1 className='text-white text-center'>Oops !!!</h1>
+                        <h1 className='text-white text-center'>404, Page not found.</h1>
+                    </div>
+                    <div className="col-sm-12 col-lg-6 shadow p-3 mb-3 bg-body rounded">
+                        <img className='img-fluid' src={Error} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
